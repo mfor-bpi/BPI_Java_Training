c515e65c14fd71c0f5ec3fd4bd2d7a4ffc5ef40a
Mark Francis O. Rivares <mforivares@headoffice.corp.bpi.com.ph>
2025-12-12 09:46:10 +0800

    upload m1 act 5

---
+++ M1_Activity5/M1Activity5.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package ph.com.bpi.m1.act5;
+
+/**
+ * 
+ */
+
+
+public class M1Activity5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int sum = 0;
+
+        for (int i = 1; i <= 50; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+
+	}
+
+}
