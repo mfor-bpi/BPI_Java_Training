87d428be65776b8996c9e38c485a3ba8bcbb2df3
Mark Francis O. Rivares <mforivares@headoffice.corp.bpi.com.ph>
2025-12-12 09:49:25 +0800

    upload m2 act 2

---
+++ M2_Activity2/M2Activity2.java
@@ -0,0 +1,13 @@
+package ph.com.bpi.m2.act2;
+
+public class M2Activity2 {
+
+	public static void main(String[] args) {
+
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(sum));		
+
+	}
+
+}
---
+++ M2_Activity2/WhiteBoard.java
@@ -0,0 +1,12 @@
+package ph.com.bpi.m2.act2;
+
+public class WhiteBoard {
+
+	int addNumbers(int addend1, int addend2) {
+		
+	int sum = addend1 + addend2;
+	return sum;
+
+	}
+	
+}
