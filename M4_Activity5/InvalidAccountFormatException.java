d8d34cccfd997041e2e21f92a02f17f35ba9b416
Mark Francis O. Rivares <mforivares@headoffice.corp.bpi.com.ph>
2026-01-10 03:54:52 +0800

    upload m4 act 5

---
+++ M4_Activity5/InvalidAccountFormatException.java
@@ -0,0 +1,9 @@
+package ph.com.bpi.m4.act5;
+
+public class InvalidAccountFormatException extends RuntimeException {
+
+	public InvalidAccountFormatException(String message) {
+        super(message);
+    }
+	
+}
